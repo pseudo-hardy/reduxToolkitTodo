2a0b234253bd7b9badfa4bfaf8f7b43ba1343abc
pseudo-hardy <hardikgupta052@gmail.com>
2025-07-25 17:18:13 +0530

    full project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddTodos from './components/AddTodos'
+import Todos from './components/Todos'
+
+function App() {
+
+
+  return (
+    <>
+    <h1>LEARN REDUX </h1>
+    <AddTodos />
+    <Todos/>
+    </>
+  )
+}
+
+export default App
